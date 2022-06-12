579996b9f594bdbc5b7f281bc2f41fc24905eb39
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-06-12 12:22:26 +0000

    Add data for the eval command

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -1,4 +1,12 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+
 module.exports = {
-    data
-}
+    data: new SlashCommandBuilder()
+        .setName('eval')
+        .setDescription('Evaluates a line of code and replies with the result.')
+        .addStringOption(option => option.setName('code').setDescription('The code to evaluate.').setRequired(true)),
+    permissions: ['OWNER'],
+    async execute (client, interaction) {
+
+    }
+};
