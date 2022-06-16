6e7a31defd5ba9b5ab0febbf46f8e072773d56df
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-06-16 12:51:44 +0000

    Add shuffle command

---
+++ commands/music/shuffle.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('shuffle')
+        .setDescription('Shuffle the queue.'),
+    permissions: [],
+    checks: ['PLAYING', 'IN_VC', 'SAME_VC'],
+    async execute (client, interaction, lava, dispatcher) {
+        if (!lava) return interaction.reply('No nodes connected.');
+        dispatcher.queue = dispatcher.queue.sort(() => Math.random() - 0.5);
+        const embed = new MessageEmbed()
+            .setDescription(`Shuffled ${dispatcher.queue.length} tracks.`)
+            .setColor(client.config.color);
+        interaction.reply({ embeds: [embed] });
+    }
+};
