a9ca30c9a913131b000a1d37a42ad9f0c6567ebc
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-06-16 12:48:00 +0000

    Add a status and activity

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,5 @@
 module.exports = async client => {
     client.logger.info(`Logged in as ${client.user.tag}!`);
+    client.user.setStatus('dnd');
+    client.user.setActivity({ name: 'you', type: 'WATCHING'});
 };
