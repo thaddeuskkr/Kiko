a558d4778b305e10340e9f46b6af76287b439d9a
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-06-16 12:46:51 +0000

    Add resume command

---
+++ commands/music/resume.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('resume')
+        .setDescription('Resume the currently paused track.'),
+    permissions: [],
+    checks: ['PLAYING', 'IN_VC', 'SAME_VC'],
+    async execute (client, interaction, lava, dispatcher) {
+        if (!lava) return interaction.reply('No nodes connected.');
+        const now = dispatcher.current;
+        const embed = new MessageEmbed()
+            .setDescription(`Resumed **${now.info.title}** by **${now.info.author}**`)
+            .setColor(client.config.color);
+        dispatcher.player.setPaused(false);
+        interaction.reply({ embeds: [embed] });
+    }
+};
