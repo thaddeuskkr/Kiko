2ff74b8783b0d43e07080c2e6d399d6ecf9b5574
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-06-17 04:16:08 +0000

    Delete unused util file

--- util/embeds.js
+++
@@ -1,9 +0,0 @@
-const { MessageEmbed } = require('discord.js');
-module.exports = {
-    insufficientPermissions: (user) => { 
-        return new MessageEmbed()
-            .setColor('RED')
-            .setAuthor({ name: 'Insufficient permissions', iconURL: user.avatarURL({ size: 4096 }) })
-            .setDescription('You do not have sufficient permissions to execute this command.');
-    }
-};
