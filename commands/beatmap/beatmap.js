f82bab30f02d680b14984a5e131c8da0356f30e9
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-07-01 18:53:55 +0000

    [bm] Add recent command, working

--- commands/beatmap/beatmap.js
+++ commands/beatmap/beatmap.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed, MessageButton } = require('discord.js');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const prettyms = require('pretty-ms');
+const _ = require('lodash');
 
 const baseURL = 'https://api.beatmap.tk';
 
@@ -15,6 +16,15 @@ module.exports = {
             .addStringOption(option => option
                 .setName('user')
                 .setDescription('The user name / ID.')))
+            
+        .addSubcommand(subcommand => subcommand
+            .setName('link')
+            .setDescription('Links your Discord account to an account on the beatmap server.')
+            .addStringOption(option => option
+                .setName('user')
+                .setDescription('The user name / ID.')
+                .setRequired(true)))
+                
         .addSubcommand(subcommand => subcommand
             .setName('recent')
             .setDescription('Shows recent plays for a specified game mode for a user.')
@@ -33,34 +43,18 @@ module.exports = {
                     { name: 'osu!taiko (Relax)', value: '5' },
                     { name: 'osu!catch (Relax)', value: '6' },
                     { name: 'osu! (Autopilot)', value: '8' }
-                )))
-        .addSubcommand(subcommand => subcommand
-            .setName('link')
-            .setDescription('Links your Discord account to an account on the beatmap server.')
-            .addStringOption(option => option
-                .setName('user')
-                .setDescription('The user name / ID.')
-                .setRequired(true))),
+                ))
+            .addBooleanOption(option => option
+                .setName('showfailed')
+                .setDescription('Whether to show failed plays.'))),
     permissions: [],
     checks: [],
     async execute (client, interaction) {
         const subcommand = interaction.options.getSubcommand();
         if (subcommand === 'userinfo') {
-            const scope = 'all';
-            const linkedId = await client.db.get(`beatmap-linked-${interaction.user.id}`);
-            if (!linkedId && !interaction.options?.getString('user')) {
-                return interaction.reply('You did not specify a player, and there are no linked accounts to your Discord account.');
-            }
-            let user;
-            if (linkedId) user = linkedId;
-            else user = interaction.options.getString('user');
-            let u = await request('/get_player_info', `id=${user}&scope=${scope}`);
-            if (!u.status) u = await request('/get_player_info', `name=${user}&scope=${scope}`);
-            if (u.status !== 'success') {
-                return interaction.reply('**Player not found.** Please check that you spelled the name correctly or entered a valid ID.');
-            }
-            const player = u.player.info;
-            let stats = u.player.stats;
+            const u = await searchUser(interaction, 'all');
+            const player = u.info;
+            let stats = u.stats;
             const embedArr = [];
             const embed1 = new MessageEmbed()
                 .setAuthor({ name: `${player.name}`, iconURL: `https://countryflagsapi.com/png/${player.country}` })
@@ -152,8 +146,6 @@ module.exports = {
                     .setStyle('PRIMARY')
             ];
             client.util.pagination(interaction, embedArr, buttons);
-        } else if (subcommand === 'recent') {
-            return interaction.reply('This command is not ready for use.');
         } else if (subcommand === 'link') {
             const user = interaction.options.getString('user');
             const linkedId = await client.db.get(`beatmap-linked-${interaction.user.id}`);
@@ -172,6 +164,87 @@ module.exports = {
             await client.db.set(`beatmap-linked-${interaction.user.id}`, u.player.info.id);
             interaction.member.roles.add('992398172347453510');
             return interaction.reply(`**Successfully linked beatmap.tk account!** You may now use commands without specifying a \`user\` argument.\n\`${interaction.user.tag} ↔ ${u.player.info.name}\``);
+        } else if (subcommand === 'recent') {
+            const u = await searchUser(interaction);
+            const gamemode = interaction.options.getString('gamemode') || 3;
+            let tgamemode;
+            if (gamemode == 0) tgamemode = 'osu!';
+            if (gamemode == 1) tgamemode = 'osu!taiko';
+            if (gamemode == 2) tgamemode = 'osu!catch';
+            if (gamemode == 3) tgamemode = 'osu!mania';
+            if (gamemode == 4) tgamemode = 'osu! (Relax)';
+            if (gamemode == 5) tgamemode = 'osu!taiko (Relax)';
+            if (gamemode == 6) tgamemode = 'osu!catch (Relax)';
+            if (gamemode == 7) tgamemode = 'osu! (Autopilot)';
+            let recent = await request('/get_player_scores', `id=${u.info.id}&scope=recent&mode=${gamemode}&limit=${interaction.options.getInteger('limit') || '45'}`);
+            if (recent.status && recent.status !== 'success') return interaction.reply(`Failed to get recent scores for **${u.info.name} (${u.info.id})**.`);
+            if (interaction.options.getBoolean('showfailed') == false) {
+                recent = recent.scores.filter((score) => score.grade !== 'F' );
+            } else {
+                recent = recent.scores;
+            }
+            let chunked = _.chunk(recent, 5);
+            let embedArr = [];
+            for (let i = 0; i < chunked.length; i++) {
+                let text = [];
+                for (let e = 0; e < chunked[i].length; e++) {
+                    let score = chunked[i][e];
+                    const date = new Date(score.play_time);
+                    const playTime = date.toLocaleString('en-US', {
+                        hour12: false,
+                        timeZone: 'Asia/Singapore',
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        second: '2-digit'
+                    });
+                    text.push(`**[${score.beatmap.title} - ${score.beatmap.artist} (mapped by ${score.beatmap.creator})](https://osu.ppy.sh/beatmapsets/${score.beatmap.set_id})** [\`${score.beatmap.diff.toFixed(2)}☆\`]`);
+                    text.push(`\`${score.grade}\` | \`${score.pp}PP\` | **Score:** \`${score.score}\` | **Accuracy:** \`${score.acc.toFixed(2)}%\``);
+                    text.push(`**Max combo:** \`${score.max_combo}\` | **Mods (bits):** \`${score.mods}\``);
+                    text.push(`**300:** \`${score.n300 + score.ngeki + score.nkatu}\` | **100:** \`${score.n100}\` | **50:** \`${score.n50}\` | **Misses:** \`${score.nmiss}\``);
+                    text.push(`**Date:** \`${playTime}\`\n**Time elapsed:** \`${prettyms(score.time_elapsed, { colonNotation: true, millisecondsDecimalDigits: 0, secondsDecimalDigits: 0 })}\` | **ID:** \`${score.id}\``);
+                    text.push('--------------');
+                }
+                
+                const embed = new MessageEmbed()
+                    .setAuthor({ name: `${u.info.name}`, iconURL: `https://countryflagsapi.com/png/${u.info.country}` })
+                    .setThumbnail(`https://a.beatmap.tk/${u.info.id}`)
+                    .setTitle(`Recent plays - ${tgamemode}`)
+                    .setURL(`https://beatmap.tk/u/${u.info.id}`)
+                    .setColor(client.config.color)
+                    .setImage('https://i.imgur.com/OYxjnMX.gif')
+                    .setDescription(text.join('\n'));
+                embedArr.push(embed);
+            }
+            const buttons = [
+                new MessageButton()
+                    .setCustomId('prev')
+                    .setLabel('Previous')
+                    .setStyle('PRIMARY'), 
+                new MessageButton()
+                    .setCustomId('next')
+                    .setLabel('Next')
+                    .setStyle('PRIMARY')
+            ];
+            client.util.pagination(interaction, embedArr, buttons);
+        }
+        async function searchUser(interaction, scope) {
+            if (!scope) scope = 'all';
+            const linkedId = await client.db.get(`beatmap-linked-${interaction.user.id}`);
+            if (!linkedId && !interaction.options?.getString('user')) {
+                return interaction.reply('You did not specify a player, and there are no linked accounts to your Discord account.');
+            }
+            let user;
+            if (linkedId && !interaction.options.getString('user')) user = linkedId;
+            else user = interaction.options.getString('user');
+            let u = await request('/get_player_info', `id=${user}&scope=${scope}`);
+            if (!u.status) u = await request('/get_player_info', `name=${user}&scope=${scope}`);
+            if (u.status !== 'success') {
+                return interaction.reply('**Player not found.** Please check that you spelled the name correctly or entered a valid ID.');
+            }
+            return u.player;
         }
         async function request(endpoint, args) {
             const response = await fetch(`${baseURL}${endpoint}?${args}`);
